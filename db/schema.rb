0bc88ab16233518d3cf2360aacf14a4422bf7123
Svetlana Solodovnikova <svslight@yandex.ru>
2020-09-28 18:33:32 +0500

    Added model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_055451) do
+ActiveRecord::Schema.define(version: 2020_09_28_125927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body", null: false
+    t.boolean "correct", default: false
+    t.bigint "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title", null: false
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 2020_09_28_055451) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
