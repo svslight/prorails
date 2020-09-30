5108304366e935c8f2fbd3a8c36ed260f2ea4fd8
Svetlana Solodovnikova <svslight@yandex.ru>
2020-10-01 04:43:37 +0500

    Fixed. Test for methods that contain code: create, update, destroy

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,64 +3,49 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
   let(:question) { create(:question) }
 
-  describe 'GET #index' do
-    let(:questions) { create_list(:question, 3) }
-    before { get :index }
+  # describe 'GET #index' do
+  #   let(:questions) { create_list(:question, 3) }
+  #   before { get :index }
 
-    it 'populates an array of all questions' do
-      expect(assigns(:questions)).to match_array(questions)
-    end
+  #   it 'populates an array of all questions' do
+  #     expect(assigns(:questions)).to match_array(questions)
+  #   end
 
-    it 'renders index view' do
-      expect(response).to render_template :index
-    end
-  end
+  #   it 'renders index view' do
+  #     expect(response).to render_template :index
+  #   end
+  # end
 
-  describe 'GET #show' do    
-    before { get :show, params: { id: question } }
+  # describe 'GET #show' do    
+  #   before { get :show, params: { id: question } }
 
-    # it 'assigns the requested question to @question' do
-    #   expect(assigns(:question)).to eq question
-    # end
+  #   it 'renders show view' do
+  #     expect(response).to render_template :show
+  #   end
+  # end
 
-    it 'renders show view' do
-      expect(response).to render_template :show
-    end
-  end
+  # describe 'Get #new' do
+  #   before { get :new }
 
-  describe 'Get #new' do
-    before { get :new }
+  #   it 'renders new view' do
+  #     expect(response).to render_template :new
+  #   end
+  # end
 
-    # it 'assigns a new Question to @question' do
-    #   expect(assigns(:question)).to be_a_new(Question)
-    # end
+  # describe "GET #edit" do
+  #   before { get :edit, params: { id: question } }
 
-    it 'renders new view' do
-      expect(response).to render_template :new
-    end
-  end
-
-  describe "GET #edit" do
-    before { get :edit, params: { id: question } }
-
-    # it 'assigns the requested question to @question' do
-    #   expect(assigns(:question)).to eq question
-    # end
-
-    it 'renders edit view' do
-      expect(response).to render_template :edit
-    end    
-  end
+  #   it 'renders edit view' do
+  #     expect(response).to render_template :edit
+  #   end    
+  # end
 
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'save a new question in the database' do    
         count = Question.count
 
-        # post :create, params: { question: { title: '123', body: '123' } }                
-        # post :create, params: { question: attributes_for(:question) } 
         expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
-        # expect(Question.count).to eq count + 1
       end
 
       it 'redirects to show view' do
@@ -82,8 +67,7 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'PATCH #update' do
-    context 'with valid attributes' do
-      
+    context 'with valid attributes' do      
       it 'assigns the requested question to @question' do
         patch :update, params: { id: question, question: attributes_for(:question) }
         expect(assigns(:question)).to eq question
