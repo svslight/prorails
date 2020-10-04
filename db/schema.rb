33cd72d692935b1e2466087ac9035caa8e1c3546
Svetlana Solodovnikova <svslight@yandex.ru>
2020-10-04 15:01:23 +0500

    Added foreign_key to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_004003) do
+ActiveRecord::Schema.define(version: 2020_10_04_032838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_10_02_004003) do
     t.bigint "question_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_answers_on_author_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
@@ -29,6 +31,8 @@ ActiveRecord::Schema.define(version: 2020_10_02_004003) do
     t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_questions_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +48,6 @@ ActiveRecord::Schema.define(version: 2020_10_02_004003) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users", column: "author_id"
+  add_foreign_key "questions", "users", column: "author_id"
 end
