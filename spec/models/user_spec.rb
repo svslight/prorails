13c0608ca101a222a3143bd33b8dd165c7d131cc
Svetlana Solodovnikova <svslight@yandex.ru>
2020-10-04 15:19:33 +0500

    Added checking for author

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,20 +4,44 @@ RSpec.describe User, type: :model do
   it { should have_many(:author_questions) }
   it { should have_many(:author_answers) }
 
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :password }
+  let(:author) { create(:user) }
+  let(:author_questions) { create(:question, author: author) }
+  let(:author_answers) { create(:answer, question: author_questions, author: author) }
 
-  describe 'author?' do
-    let(:author) { create(:user) }
+  let(:other_author) { create(:user) }
+  let(:other_question) { create(:question, author: other_author) }
+  let(:other_answer) { create(:answer, question: other_question, author: other_author) }
 
-    it 'returns true if the user is the author of the question' do
-      question = create(:question, author: author)
-      expect(author).to be_author(question)
-    end
-
-    it 'returns false if the user is not the author of the question' do
-      question = create(:question)
-      expect(author).not_to be_author(question)
-    end
+  it 'User is author of question' do
+    expect(author).to be_author(author_questions)
   end
+
+  it 'User is not author of question' do
+    expect(author).not_to be_author(other_question)
+  end
+
+  it 'User is author of answer' do
+    expect(author).to be_author(author_answers)
+  end
+
+  it 'User is not author of answer' do
+    expect(author).not_to be_author(other_answer)
+  end
+
+  # it { should validate_presence_of :email }
+  # it { should validate_presence_of :password }
+
+  # describe 'author?' do
+  #   let(:author) { create(:user) }
+
+  #   it 'returns true if the user is the author of the question' do
+  #     question = create(:question, author: author)
+  #     expect(author).to be_author(question)
+  #   end
+
+  #   it 'returns false if the user is not the author of the question' do
+  #     question = create(:question)
+  #     expect(author).not_to be_author(question)
+  #   end
+  # end
 end
