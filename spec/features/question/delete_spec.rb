c652a31b0220e66c56791fef93aa3a26c8702fb7
Svetlana Solodovnikova <svslight@yandex.ru>
2020-10-04 14:43:15 +0500

    Story_fixed: User trying delete question (task6)

--- spec/features/question/delete_spec.rb
+++ spec/features/question/delete_spec.rb
@@ -5,25 +5,41 @@ feature 'User can delete his question', %q(
   I would like to be able to delete my question
 ) do
 
-  given(:user_author) { create(:user) }
+  given(:author) { create(:user) }
+  given!(:question) { create(:question, author: author) }
+
+  given(:other_user) { create(:user) }
+  given!(:other_question) { create(:question, author: other_user) }
 
   describe 'Authenticated user' do
     background do
-      sign_in(user_author)
+      sign_in(author)
     end
 
     scenario 'can delete his question' do
-      questions = create_list(:question, 4, author: user_author)
-      visit question_path(questions.first)
+      expect(page).to have_content question.title
   
-      click_link 'Delete'
+      click_on  'Delete'
   
       expect(page).to have_content 'Your question was successfully deleted.'
-      expect(page).to have_no_content questions.first.title
+      expect(page).not_to have_content question.title
+    end
+
+    scenario 'wants to delete another user question' do
+      within("#question_#{other_question.id}") do
+        expect(page).to have_content other_question.title
+        expect(page).to_not have_link 'Delete'
+      end    
     end
   end
   
-  scenario 'wants to delete another user question'
-  scenario 'Unauthenticated user tries to delete a question'
-
+  describe 'Unauthenticated user' do
+    scenario 'trying to delete question' do
+      visit questions_path
+      within("#question_#{question.id}") do
+        expect(page).to have_content question.title
+        expect(page).to_not have_link 'Delete'
+      end
+    end
+  end
 end
--- spec/features/question/view_spec.rb
+++ spec/features/question/view_spec.rb
@@ -4,23 +4,26 @@ feature 'User can see a list of questions', %q(
   I'd like to see the list of questions.
 ) do
   
+  given(:author) { create(:user) }
+  given(:question) { create(:question, author: author) }
+
   scenario 'User sees a list of questions' do
-    questions = create_list(:question, 3)
+    questions = create_list(:question, 3, author: author)
     visit questions_path
 
     questions.each { |question| expect(page).to have_content(question.title) }
   end
-
 end
 
 feature 'User can view the question and answers to it', %q(
   I'd like to view the question and answers to it.
 ) do
 
-  given(:question) { create(:question) }
+  given(:author) { create(:user) }
+  given(:question) { create(:question, author: author) }
 
   scenario 'User can view the question and its answers' do
-    answers = create_list(:answer, 4, question: question)
+    answers = create_list(:answer, 4, question: question, author: author)
     visit question_path(question)
 
     expect(page).to have_content question.title
