4c6dd82ad62737abf6a8326dfd05ab0cc4937dc9
Svetlana Solodovnikova <svslight@yandex.ru>
2020-10-07 12:51:42 +0500

    Story: Only the author can edit their answer (task7-ajax)

--- spec/features/answers/edit_spec.rb
+++ spec/features/answers/edit_spec.rb
@@ -6,22 +6,25 @@ feature 'User can edit his answer', %q{
   I'd like ot be able to edit my answer
 } do
 
-  given!(:author) { create(:user) }
-  given!(:question) { create(:question, author: author) }
-  given!(:answer) { create(:answer, question: question, author: author) }
+  given!(:user_author) { create(:user) }
+  given!(:question) { create(:question, author: user_author) }
+  given!(:answer) { create(:answer, question: question, author: user_author) }
+
+  given!(:other_user) { create(:user) }
+  given!(:other_answer) { create(:answer, question: question, author: other_user) }
 
   scenario 'Unauthenticated can not edit answer' do
     visit question_path(question)
-
     expect(page).to_not have_link 'Edit'
   end
 
   describe 'Authenticated user', js: true do
-    scenario 'edits his answer' do
-      sign_in author
+    background do
+      sign_in(user_author)
       visit question_path(question)
+    end
 
-      # save_and_open_page
+    scenario 'edits his answer' do
       click_on 'Edit'
 
       within '.answers' do
@@ -34,7 +37,22 @@ feature 'User can edit his answer', %q{
       end
     end
 
-    scenario 'edits his answer with errors'
-    scenario "tries to edit other user's question"
+    scenario 'edits his answer with errors' do 
+      click_on 'Edit'
+
+      within '.answers' do
+        fill_in 'Your answer', with: ''
+        click_on 'Save'
+
+        expect(page).to have_content answer.body
+      end
+      expect(page).to have_content "Body can't be blank"
+    end
+
+    scenario "tries to edit other user's question" do
+      within "#answer_#{other_answer.id}" do
+        expect(page).to_not have_link 'Edit'
+      end
+    end
   end
 end
