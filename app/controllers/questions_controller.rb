32c6573571372da61fdc5884d0d3d4f8c86f4d95
Svetlana Solodovnikova <svslight@yandex.ru>
2020-10-07 08:45:05 +0500

    Fixed: Only the author can edit their question (task7)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,11 +16,7 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    if question.update(question_params)
-      redirect_to question_path(question) 
-    else
-      render :edit
-    end
+    question.update(question_params) if current_user.author?(question)
   end
 
   def destroy
