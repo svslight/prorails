64778e254baedba600e16e47017ba4b608dd50f0
Svetlana Solodovnikova <svslight@yandex.ru>
2020-10-15 09:44:22 +0500

    Story: Author can edit a question with attached file (task8)

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -51,5 +51,17 @@ feature 'User can edit his question', %q{
         expect(page).to_not have_link 'Edit'
       end
     end
+
+    scenario 'edit a question with attached file' do
+      within "#question-#{question.id}" do
+        click_on 'Edit'
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+      end
+
+      visit question_path(question)
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
   end
 end
