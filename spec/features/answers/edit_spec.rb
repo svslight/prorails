e7d6b3cfe05dc97880544856e2b34fcc4064b357
Svetlana Solodovnikova <svslight@yandex.ru>
2020-10-15 10:20:19 +0500

    Story: Author can edit a answer with attached file (task8)

--- spec/features/answers/edit_spec.rb
+++ spec/features/answers/edit_spec.rb
@@ -54,5 +54,16 @@ feature 'User can edit his answer', %q{
         expect(page).to_not have_link 'Edit'
       end
     end
+
+    scenario 'edit answer with attached file' do
+      within "#answer-#{answer.id}" do
+        click_on 'Edit'
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 end
