e3e0c4ac56b3a8d5b846f0a9f0585cb3efe559b9
Svetlana Solodovnikova <svslight@yandex.ru>
2020-10-18 12:51:02 +0500

    Fixed: Validation of URL format (task9)

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,16 @@
-class Link < ApplicationRecord  
+class Link < ApplicationRecord
+  URL_FORMAT = /\A(http|https):\/\/|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?\z/ix
+
   belongs_to :linkable, polymorphic: true
   
-  validates :name, :url, presence: true
+  validates :name, presence: true
+  validates :url, presence: true, format: { with: URL_FORMAT, message: 'URL has an invalid format!!!' }
+
+  def gist?
+    url =~ /gist.github.com/
+  end
+
+  def gist_content
+    GistContentService.new(url.split('/').last).content if gist?
+  end
 end
