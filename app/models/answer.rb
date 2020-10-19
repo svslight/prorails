7282f919300e17c840cbbdc1badb2e8bfd20f2af
Svetlana Solodovnikova <svslight@yandex.ru>
2020-10-19 06:53:49 +0500

    Fixed: Author can edit link from his question (task9)

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,10 +3,9 @@ class Answer < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   has_many :links, dependent: :destroy, as: :linkable
+  accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
 
   has_many_attached :files
-
-  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   validates :body, presence: true
 
