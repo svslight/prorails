fb604e78200de6614eef418dc9a471dc94e965dd
Svetlana Solodovnikova <svslight@yandex.ru>
2020-11-01 09:58:30 +0500

    Added ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,6 @@ Rails.application.routes.draw do
   resources :attachments, only: :destroy
   resources :links, only: :destroy
   resources :rewards, only: :index
+
+  mount ActionCable.server => '/cable'
 end
