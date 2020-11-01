f686625514b09a1d9ea111a34cf9b84177c02bfe
Svetlana Solodovnikova <svslight@yandex.ru>
2020-11-01 09:59:28 +0500

    ActionCable - adding a new question

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -61,5 +61,34 @@ feature 'User can create question', %q{
     click_on 'Ask question'
     
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
-  end  
+  end
+
+  context 'multiple sessions' do
+    scenario "question appears on another user's page", js: true do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit questions_path
+      end
+
+      Capybara.using_session('guest') do
+        visit questions_path
+      end
+
+      Capybara.using_session('user') do
+        click_on 'Ask question'
+
+        fill_in 'Title', with: 'Test question'
+        fill_in 'Body', with: 'text text text'
+        click_on 'Ask'
+
+        expect(page).to have_content 'Your question successfully created.'
+        expect(page).to have_content 'Test question'
+        expect(page).to have_content 'text text text'
+      end
+
+      Capybara.using_session('guest') do
+        expect(page).to have_content 'Test question'
+      end
+    end
+  end
 end
