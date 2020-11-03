d199e70ae59ad3db8437341d292a0e40df42cf10
Svetlana Solodovnikova <svslight@yandex.ru>
2020-11-03 23:48:37 +0500

    OAuth: Authorization VK (task12)

--- spec/controllers/oauth_callbacks_controller_spec.rb
+++ spec/controllers/oauth_callbacks_controller_spec.rb
@@ -6,21 +6,9 @@ RSpec.describe OauthCallbacksController, type: :controller do
   end
 
   describe 'Github' do
-    # введем те данные которые хотим увидеть
-    # чтобы эта проверка сработала нужно сэмулировать поведение этого вызова
-    # (request.env['omniauth.auth']) таким образом чтобы он вернул указанные данные
-    # проверим что метод берет данные из нужного места 
     let(:oauth_data) { {'provider' => 'github', 'uid' => 123 } }
 
     it 'finds user from oauth data' do
-
-      # напишем параметры (аргументы) с которыми ожидаем вызов этого метода
-      # через опцию .with(oauth_data) - если ожидаем возврат то можно написать
-      # .and_return(user)
-      # тест не прошед - получили nil вместо {'provider' => 'github', 'uid' => 123 }
-      # теперь чтобы проверить что все работает правильно сэмулируем тест: 
-      # и нужно указать заглушку если вызывается с другими параметрами отличными от (omniauth.auth) 
-      # и выполняем это действие так как  оно реализовано (and_call_original)
       allow(request.env).to receive(:[]).and_call_original
       allow(request.env).to receive(:[]).with('omniauth.auth').and_return(oauth_data)
 
@@ -29,8 +17,6 @@ RSpec.describe OauthCallbacksController, type: :controller do
     end
 
     context 'user exists' do
-      # понадобиться user(создадим)
-      # указываем (!) так как он понадобится перед каждым тестом
       let!(:user) { create(:user) }
 
       before do
@@ -38,13 +24,7 @@ RSpec.describe OauthCallbacksController, type: :controller do
         get :github
       end
 
-      # делаем проверку на find_for_oauth но чтобы метод возвращал конкретное значение
-      # ставим and_return(значение которое хотим вернуть )
-      # find_for_oauth - метод вызываться не будет 
-      # респект перехватит вызов этого метода и если он будет вызван 
-      # вернет значение без выполнения метода
       it 'login user' do
-        # проверяем что польз-ль залогинен и равен пользователю который вернулся
         expect(subject.current_user).to eq user
       end
 
@@ -55,7 +35,7 @@ RSpec.describe OauthCallbacksController, type: :controller do
 
     context 'user does not exist' do
       before do
-        allow(User).to receive(:find_for_oauth) #.and_return(user)
+        allow(User).to receive(:find_for_oauth)
         get :github
       end
 
@@ -64,10 +44,52 @@ RSpec.describe OauthCallbacksController, type: :controller do
       end
 
       it 'does not login user' do
-        # объекта нет
-        expect(subject.current_user).to_not be
+        expect(subject.current_user).not_to be
       end
     end
 
+    describe 'Vkontakte' do
+      let(:oauth_data) { {'provider' => 'vkontakte', 'uid' => 123 } }
+
+      it 'finds user from oauth data' do
+        allow(request.env).to receive(:[]).and_call_original
+        allow(request.env).to receive(:[]).with('omniauth.auth').and_return(oauth_data)
+  
+        expect(User).to receive(:find_for_oauth).with(oauth_data)
+        get :vkontakte
+      end
+
+      context 'user exists' do
+        let!(:user) { create(:user) }
+
+        before do
+          allow(User).to receive(:find_for_oauth).and_return(user)
+          get :vkontakte
+        end
+
+        it 'login user' do
+          expect(subject.current_user).to eq user
+        end
+
+        it 'redirects to root path' do
+          expect(response).to redirect_to root_path
+        end
+      end
+
+      context 'user does not exist' do
+        before do
+          allow(User).to receive(:find_for_oauth)
+          get :vkontakte
+        end
+
+        it 'redirects to root path' do
+          expect(response).to redirect_to root_path
+        end
+
+        it 'does not login user' do
+          expect(subject.current_user).not_to be
+        end
+      end      
+    end
   end
 end
