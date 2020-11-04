3543d522bbd32cf032fbb69335e4395bf10062df
Svetlana Solodovnikova <svslight@yandex.ru>
2020-11-04 08:16:14 +0500

    OAuth: Authorizations (task12)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,16 +11,13 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :password }
   it { should have_many(:authorizations).dependent(:destroy) }
 
+  let!(:user) { create(:user) }
+
   describe '.find_for_oauth' do
-    let!(:user) { create(:user) }
     let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
 
-    # double - создает фэйковый объект, в качестве аргумента принимает строку с любым именем
     let(:service) { double('FindForOauth') }
 
-    # тест должен вызывать FindForOauth
-    # проверить что вызывается конструктор этого класса с определенными аргументами
-    # и созанный объект получает вызов метода call
     it 'call FindForOauth' do
       expect(FindForOauth).to receive(:new).with(auth).and_return(service)
       expect(service).to receive(:call)
@@ -29,28 +26,27 @@ RSpec.describe User, type: :model do
   end
 
   describe 'User' do
-    let(:author) { create(:user) }
-    let(:author_questions) { create(:question, author: author) }
-    let(:author_answers) { create(:answer, question: author_questions, author: author) }
+    let(:author_questions) { create(:question, author: user) }
+    let(:author_answers) { create(:answer, question: author_questions, author: user) }
   
     let(:other_author) { create(:user) }
     let(:other_question) { create(:question, author: other_author) }
     let(:other_answer) { create(:answer, question: other_question, author: other_author) }
   
     it 'User is author of question' do
-      expect(author).to be_author(author_questions)
+      expect(user).to be_author(author_questions)
     end
   
     it 'User is not author of question' do
-      expect(author).not_to be_author(other_question)
+      expect(user).not_to be_author(other_question)
     end
   
     it 'User is author of answer' do
-      expect(author).to be_author(author_answers)
+      expect(user).to be_author(author_answers)
     end
   
     it 'User is not author of answer' do
-      expect(author).not_to be_author(other_answer)
+      expect(user).not_to be_author(other_answer)
     end
   end
 
