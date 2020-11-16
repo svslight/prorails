7b65cdef4ceda03730469565641efee8c6122840
Svetlana Solodovnikova <svslight@yandex.ru>
2020-11-16 16:44:51 +0500

    Authorizations CanCanCan (task13)

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -32,10 +32,9 @@ class Ability
     can :destroy, Link, linkable: { author_id: user.id }
     can :read, Reward
  
-    # can :destroy, ActiveStorage::Attachment, record: { author_id: user.id }
-    # can :destroy, ActiveStorage::Attachment do |file|
-    #   user.author?(file.record)
-    # end
+    can :destroy, ActiveStorage::Attachment do |file|
+      user.author?(file.record)
+    end
 
     can [:vote_up, :vote_down], [Question, Answer] do |voteable|
       !user.author?(voteable) && !voteable.votes.exists?(user_id: user.id)
