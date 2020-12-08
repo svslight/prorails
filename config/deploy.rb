dad90ea7461cd5285cb9d8d65a495708f5fd9313
Svetlana Solodovnikova <svslight@yandex.ru>
2020-12-08 09:17:56 +0500

    Deploy(1-2): Capistrano Configure (Lesson 17-18)

---
+++ config/deploy.rb
@@ -0,0 +1,15 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.14.1"
+
+set :application, "prorails"
+set :repo_url, "git@github.com:svslight/prorails.git"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/prorails"
+set :deploy_user, 'deployer'
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", 'config/master.key'
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", 'storage'
